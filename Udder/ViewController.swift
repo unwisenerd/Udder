361fd3f2f563c38e378da492625f57ecfb754a3c
Andrew Leshovsky <unwisenerd@gmail.com>
2021-10-19 14:41:26 +0300

    Add viewDIdDisappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
 
 
 }
